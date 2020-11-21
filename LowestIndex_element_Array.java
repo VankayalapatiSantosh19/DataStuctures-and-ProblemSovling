380ea52d105ca45ee4daf7f74f0c69b0f1548764
SANTOSH VANKAYALAPATI <51921268+VankayalapatiSantosh19@users.noreply.github.com>
2020-11-21 21:52:11 +0530

    Add files via upload
    Finding the Lowest index of an element 'x' in an given array using Binary Search.

---
+++ LowestIndex_element_Array.java
@@ -0,0 +1,29 @@
+package Binary_Search_Thinking;
+
+import java.util.Random;
+
+public class LowestIndex_element_Array {
+	//Time:Theta(logn) space:O(1)
+	public static int lowestIndex(int[] a,int x) {
+		int l=0,r=a.length-1;
+		while(l<r) {
+			int m=(l+r)/2;
+			if(x==a[m]) r=m;
+			else if(x<a[m])r=m-1;
+			else l=m+1;
+		}
+		if(a[l]==x)return l;
+		return -1;
+	}
+	public static void main(String[] args) {
+		int n=Integer.parseInt(args[0]);
+		int a[]=Utils.sortedRandomarraywithduplicates(n); //Utils class is present in data structures folder. it generates an random arraqy with duplicates. 
+		int x=new Random().nextInt(10)+1;   //generates an random number between 1 to 10
+	    Utils.display(a);
+		System.out.println(x);
+		System.out.println(lowestIndex(a,x));//print lowest index of an element in array
+		
+
+	}
+
+}
