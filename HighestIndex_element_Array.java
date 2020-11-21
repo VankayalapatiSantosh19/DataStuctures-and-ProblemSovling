905b2e3bb287525eee2ba91be971573cd6db4d18
SANTOSH VANKAYALAPATI <51921268+VankayalapatiSantosh19@users.noreply.github.com>
2020-11-21 23:02:51 +0530

    Add files via upload
    Find Highest Index of an element 'x' in the given array

---
+++ HighestIndex_element_Array.java
@@ -0,0 +1,27 @@
+package Binary_Search_Thinking;
+import java.util.*;
+public class HighestIndex_element_Array {
+	//Time:Theta(logn) space:O(1)
+	public static int HighestIndex(int[] a,int x) {
+		int l=0,r=a.length-1;
+		while(l<r) {
+			int m=(int)Math.ceil((l+r)/2.0);
+			if(x==a[m]) l=m;
+			else if(x<a[m])r=m-1;
+			else l=m+1;
+		}
+		if(a[l]==x)return l;
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		int n=Integer.parseInt(args[0]);
+		int a[]=Utils.sortedRandomarraywithduplicates(n); //Utils class is present in data structures folder. it generates an random arraqy with duplicates. 
+		int x=new Random().nextInt(10)+1;   //generates an random number between 1 to 10
+	    Utils.display(a);
+		System.out.println(x);
+		System.out.println(HighestIndex(a,x));
+
+	}
+
+}
